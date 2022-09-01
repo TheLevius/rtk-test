3a043c9343b788838ee5b36afb3ff5d641e92269
reflevius <nikita.l@webspace.by>
2022-09-02 00:00:54 +0300

    feat: rtk-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,72 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import TodoList from './components/TodoList.jsx';
-import InputField from './components/InputField.jsx';
+// import TodoList from './components/TodoList.jsx';
+// import InputField from './components/InputField.jsx';
 import {useDispatch, useSelector} from 'react-redux';
 import { addNewTodo, fetchTodos } from './store/todoSlice.js';
+import {useAddProductMutation, useGetGoodsQuery, useDeleteProductMutation } from './store/goodsApi.js'
 
 const App = () => {
-    const [text, setText] = useState('')
-    const {status, error} = useSelector((state) => state.todos)
+    // const [text, setText] = useState('')
+    // const {status, error} = useSelector((state) => state.todos)
+    const [count, setCount] = useState('')
+    const [newProduct, setNewProduct] = useState('')
+    const { data = [], isLoading} = useGetGoodsQuery(count)
+    const [addProduct, addProductInfo] = useAddProductMutation()
+    const [deleteProduct, deleteProductInfo] = useDeleteProductMutation()
     const dispatch = useDispatch()
 
-    const addTask = () => {
-        dispatch(addNewTodo({ title: text }))
-        setText('')
+    const handleAddProduct = async () => {
+        if (newProduct) {
+            await addProduct({ name: newProduct }).unwrap()
+            setNewProduct('')
+        }
     }
+    const handleDeleteProduct = async (id) => {
+        await deleteProduct(id).unwrap()
+
+    }
+    // const addTask = () => {
+    //     dispatch(addNewTodo({ title: text }))
+    //     setText('')
+    // }
 
     useEffect(() => {
-        dispatch(fetchTodos())
+        // dispatch(fetchTodos())
     }, [dispatch])
 
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
     return (
         <div className="App">
-            <InputField text={text} handleSubmit={addTask} handleInput={setText}/>
-            { status === 'loading' && <h2>Loading...</h2>}
-            { error && <h2>An error occurred: {error}</h2>}
-            <TodoList/>
+            <div>
+                <input type="text" value={newProduct} onChange={(e) => setNewProduct(e.target.value)}/>
+                <button onClick={handleAddProduct}>Add Product</button>
+            </div>
+
+            <div>
+                <select value={count} onChange={(e) => setCount(e.target.value)}>
+                    <option value="">all</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
+            <ul>
+                {data.map((item) => (
+                    <li
+                        key={item.id}
+                        onClick={() => handleDeleteProduct(item.id)}
+                    >
+                    {item.name}
+                    </li>
+                ))}
+            </ul>
+            {/*<InputField text={text} handleSubmit={addTask} handleInput={setText}/>*/}
+            {/*{ status === 'loading' && <h2>Loading...</h2>}*/}
+            {/*{ error && <h2>An error occurred: {error}</h2>}*/}
+            {/*<TodoList/>*/}
         </div>
     )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './todoSlice.js'
+import { goodsApi } from './goodsApi.js';
+
 export default configureStore({
     reducer: {
-        todos: todoReducer
-    }
+        todos: todoReducer,
+        [goodsApi.reducerPath]: goodsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(goodsApi.middleware)
 })
